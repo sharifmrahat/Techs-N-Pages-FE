a418a640d807f1d0eeb044c7e2dff082d0bfecdd
ShareefRahat <imr4h47@gmail.com>
2022-10-30 22:45:13 +0600

    newly published books and dynamic bookDetails route is created

---
+++ pages/books/[id].js
@@ -0,0 +1,196 @@
+/* eslint-disable @next/next/no-img-element */
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+import { useState } from 'react'
+import { CheckIcon, QuestionMarkCircleIcon, StarIcon } from '@heroicons/react/20/solid'
+import { RadioGroup } from '@headlessui/react'
+import { ShieldCheckIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+ function BookDetails({data}) {
+    
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl py-12 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+        {/* Product details */}
+        <div className="lg:max-w-lg lg:self-end">
+
+          <div className="mt-4">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{data.title}</h1>
+          </div>
+
+          <section aria-labelledby="information-heading" className="mt-4">
+            <h2 id="information-heading" className="sr-only">
+              Product information
+            </h2>
+
+            <div className="flex items-center">
+              <p className="text-lg text-gray-900 sm:text-xl">{data.price}</p>
+
+              <div className="ml-4 border-l border-gray-300 pl-4">
+                <h2 className="sr-only">Reviews</h2>
+                <div className="flex items-center">
+                  <div>
+                    <div className="flex items-center">
+                      {[0, 1, 2, 3, 4].map((rating) => (
+                        <StarIcon
+                          key={rating}
+                          className={classNames(
+                            data.rating > rating ? 'text-yellow-400' : 'text-gray-300',
+                            'h-5 w-5 flex-shrink-0'
+                          )}
+                          aria-hidden="true"
+                        />
+                      ))}
+                    </div>
+                    <p className="sr-only">{data.rating} out of 5 stars</p>
+                  </div>
+                  <p className="ml-2 text-sm text-gray-500">{data.rating} reviews</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-4 space-y-6">
+              <p className="text-base text-gray-500">{data.desc}</p>
+            </div>
+
+            <div className="mt-6 flex items-center">
+              <CheckIcon className="h-5 w-5 flex-shrink-0 text-green-500" aria-hidden="true" />
+              <p className="ml-2 text-sm text-gray-500">In stock and ready to ship</p>
+            </div>
+          </section>
+        </div>
+
+        {/* Product image */}
+        <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
+          <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg">
+            <img src={data.image} alt={data.title} className="h-full w-full object-cover object-center" />
+          </div>
+        </div>
+
+        {/* Product form */}
+        <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
+          <section aria-labelledby="options-heading">
+            <h2 id="options-heading" className="sr-only">
+              Product options
+            </h2>
+
+            <form>
+              <div className="sm:flex sm:justify-between">
+                {/* Size selector */}
+                {/* <RadioGroup value={selectedSize} onChange={setSelectedSize}>
+                  <RadioGroup.Label className="block text-sm font-medium text-gray-700">Size</RadioGroup.Label>
+                  <div className="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
+                    {product.sizes.map((size) => (
+                      <RadioGroup.Option
+                        as="div"
+                        key={size.name}
+                        value={size}
+                        className={({ active }) =>
+                          classNames(
+                            active ? 'ring-2 ring-indigo-500' : '',
+                            'relative block cursor-pointer rounded-lg border border-gray-300 p-4 focus:outline-none'
+                          )
+                        }
+                      >
+                        {({ active, checked }) => (
+                          <>
+                            <RadioGroup.Label as="p" className="text-base font-medium text-gray-900">
+                              {size.name}
+                            </RadioGroup.Label>
+                            <RadioGroup.Description as="p" className="mt-1 text-sm text-gray-500">
+                              {size.description}
+                            </RadioGroup.Description>
+                            <div
+                              className={classNames(
+                                active ? 'border' : 'border-2',
+                                checked ? 'border-indigo-500' : 'border-transparent',
+                                'pointer-events-none absolute -inset-px rounded-lg'
+                              )}
+                              aria-hidden="true"
+                            />
+                          </>
+                        )}
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup> */}
+              </div>
+              {/* <div className="mt-4">
+                <a href="#" className="group inline-flex text-sm text-gray-500 hover:text-gray-700">
+                  <span>What size should I buy?</span>
+                  <QuestionMarkCircleIcon
+                    className="ml-2 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                    aria-hidden="true"
+                  />
+                </a>
+              </div> */}
+              <div className="mt-10">
+                <button
+                  type="submit"
+                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                >
+                  Add to bag
+                </button>
+              </div>
+              {/* <div className="mt-6 text-center">
+                <a href="#" className="group inline-flex text-base font-medium">
+                  <ShieldCheckIcon
+                    className="mr-2 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                    aria-hidden="true"
+                  />
+                  <span className="text-gray-500 hover:text-gray-700">Lifetime Guarantee</span>
+                </a>
+              </div> */}
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export async function getStaticProps(ctx){
+
+    const {params} = ctx
+    const res = await fetch(`https://api.itbook.store/1.0/books/${params?.id}`)
+    const data = await res.json()
+    return {
+        props:{
+            data
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const res = await fetch("https://api.itbook.store/1.0/new")
+    const data = await res.json()
+    const paths = data.books.map(book => {
+        return {
+            params: {id: book.isbn13}
+        }
+    })
+    return {
+      paths: paths,
+      fallback: false, // can also be true or 'blocking'
+    }
+  }
+
+  export default BookDetails
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
+import DisplayBooks from '../components/DisplayBooks'
 import HeroSection from '../components/HeroSection'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+ function Home({data}) {
+  console.log(data?.books)
   return (
     <div>
       <main>
         <HeroSection></HeroSection>
+        <DisplayBooks books={data?.books.slice(0,6)}></DisplayBooks>
       </main>
     </div>
   )
 }
+
+export async function getStaticProps(ctx){
+
+  const res = await fetch("https://api.itbook.store/1.0/new")
+  const data = await res.json()
+  return {
+    props:{
+      data: data
+    }
+  }
+}
+
+export default Home;
