08dfe45753edc8bdd6379fc6b4baf55290837f58
ShareefRahat <imr4h47@gmail.com>
2022-11-04 23:21:25 +0600

    book details page dark mode and style is updated

--- pages/books/[id].js
+++ pages/books/[id].js
@@ -27,16 +27,16 @@ function classNames(...classes) {
  function BookDetails({data}) {
     console.log(data)
   return (
-    <div className="bg-white px-5 lg:px-0">
-      <div className="mx-auto max-w-2xl py-12 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+    <div className="bg-white dark:bg-slate-700 text-slate-900 dark:text-slate-200 px-5 lg:px-0 font-primary pb-48 lg:pt-6">
+      <div className="mx-auto max-w-2xl lg:max-w-7xl flex flex-col-reverse lg:flex-row justify-evenly">
         {/* Product details */}
         <div className="lg:max-w-lg lg:self-end">
 
           <div className="mt-4">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{data.title}</h1>
+            <h1 className="text-3xl font-bold tracking-tight sm:text-4xl">{data.title}</h1>
           </div>
 
-          <section aria-labelledby="information-heading" className="my-4 text-slate-600">
+          <section aria-labelledby="information-heading" className="my-4 text-slate-600 dark:text-slate-400">
             <h2 id="information-heading" className="">
             Author: {data.authors}
             </h2>
@@ -48,7 +48,7 @@ function classNames(...classes) {
             </h2>
 
             <div className="flex items-center mt-4">
-              <p className="text-lg text-gray-900 sm:text-xl">{data.price}</p>
+              <p className="text-lg  sm:text-xl">{data.price}</p>
 
               <div className="ml-4 border-l border-gray-300 pl-4">
                 <h2 className="sr-only">Reviews</h2>
@@ -68,106 +68,47 @@ function classNames(...classes) {
                     </div>
                     <p className="sr-only">{data.rating} out of 5 stars</p>
                   </div>
-                  <p className="ml-2 text-sm text-gray-500">{data.rating} reviews</p>
+                  <p className="ml-2 text-sm text-gray-500 dark:text-slate-400">{data.rating} reviews</p>
                 </div>
               </div>
             </div>
 
             <div className="mt-4 space-y-6">
-              <p className="text-base text-gray-500">{data.desc}</p>
+              <p className="text-base text-gray-500 dark:text-slate-400">{data.desc}</p>
             </div>
 
             <div className="mt-6 flex items-center">
               <CheckIcon className="h-5 w-5 flex-shrink-0 text-green-500" aria-hidden="true" />
-              <p className="ml-2 text-sm text-gray-500">In stock and ready to ship</p>
+              <p className="ml-2 text-sm text-gray-500 dark:text-slate-400">In stock and ready to ship</p>
             </div>
           </section>
-        </div>
-
-        {/* Product image */}
-        <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
-          <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg">
-            <img src={data.image} alt={data.title} className="h-full w-full object-cover object-center" />
-          </div>
-        </div>
-
-        {/* Product form */}
-        <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
+          <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
           <section aria-labelledby="options-heading">
             <h2 id="options-heading" className="sr-only">
               Product options
             </h2>
-
-            <form>
-              <div className="sm:flex sm:justify-between">
-                {/* Size selector */}
-                {/* <RadioGroup value={selectedSize} onChange={setSelectedSize}>
-                  <RadioGroup.Label className="block text-sm font-medium text-gray-700">Size</RadioGroup.Label>
-                  <div className="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
-                    {product.sizes.map((size) => (
-                      <RadioGroup.Option
-                        as="div"
-                        key={size.name}
-                        value={size}
-                        className={({ active }) =>
-                          classNames(
-                            active ? 'ring-2 ring-indigo-500' : '',
-                            'relative block cursor-pointer rounded-lg border border-gray-300 p-4 focus:outline-none'
-                          )
-                        }
-                      >
-                        {({ active, checked }) => (
-                          <>
-                            <RadioGroup.Label as="p" className="text-base font-medium text-gray-900">
-                              {size.name}
-                            </RadioGroup.Label>
-                            <RadioGroup.Description as="p" className="mt-1 text-sm text-gray-500">
-                              {size.description}
-                            </RadioGroup.Description>
-                            <div
-                              className={classNames(
-                                active ? 'border' : 'border-2',
-                                checked ? 'border-indigo-500' : 'border-transparent',
-                                'pointer-events-none absolute -inset-px rounded-lg'
-                              )}
-                              aria-hidden="true"
-                            />
-                          </>
-                        )}
-                      </RadioGroup.Option>
-                    ))}
-                  </div>
-                </RadioGroup> */}
-              </div>
-              {/* <div className="mt-4">
-                <a href="#" className="group inline-flex text-sm text-gray-500 hover:text-gray-700">
-                  <span>What size should I buy?</span>
-                  <QuestionMarkCircleIcon
-                    className="ml-2 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                    aria-hidden="true"
-                  />
-                </a>
-              </div> */}
-              <div className="mt-10">
+            <div className="mt-10">
                 <button
                   type="submit"
-                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 dark:bg-indigo-40s0 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 dark:hover:bg-indigo-500 focus:outline-none"
                 >
                   Add to bag
                 </button>
               </div>
-              {/* <div className="mt-6 text-center">
-                <a href="#" className="group inline-flex text-base font-medium">
-                  <ShieldCheckIcon
-                    className="mr-2 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                    aria-hidden="true"
-                  />
-                  <span className="text-gray-500 hover:text-gray-700">Lifetime Guarantee</span>
-                </a>
-              </div> */}
-            </form>
           </section>
         </div>
+        </div>
+        
+
+        {/* Product image */}
+        <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
+          <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg">
+            <img src={data.image} alt={data.title} className="h-full w-full lg:h-[500px]" />
+          </div>
+        </div>
+
+        {/* Product form */}
+        
       </div>
     </div>
   )
