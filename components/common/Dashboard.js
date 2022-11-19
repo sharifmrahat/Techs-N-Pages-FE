fcd163fcc4261c32005afebda0066d2a8a80889f
sharifmrahat <sharifmrahat@gmail.com>
2022-11-19 22:05:54 +0600

    dashboard route conditon is changed

--- components/common/Dashboard.js
+++ components/common/Dashboard.js
@@ -46,8 +46,8 @@ export default function Dashboard({component}) {
     if (currentUser.length === 0) {
       setRefetch(true);
     }
-    if (currentUser.success) {
-      router.push({pathname: router.pathname});
+    if (!currentUser.success) {
+      router.push('/');
     }
     else{
       router.push('/')
