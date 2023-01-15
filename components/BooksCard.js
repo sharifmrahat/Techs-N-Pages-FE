0c4236e48985c0bec72540c6b499ddc1e18a1ed8
sharifmrahat <sharifmrahat@gmail.com>
2023-01-15 20:00:56 +0600

    books card design is updated

--- components/BooksCard.js
+++ components/BooksCard.js
@@ -19,11 +19,11 @@ export default function BooksCard({books, heading}) {
         </> : <>
         <div className="mx-auto py-12  max-w-2xl lg:max-w-7xl  bg-white dark:bg-slate-700">
         <h2 className="text-xl lg:text-2xl font-bold pb-10">{heading}</h2>
-        <div className="grid gap-20 lg:gap-16 lg:grid-cols-5 mx-5 lg:mx-0">
+        <div className="grid gap-20 lg:gap-16 md:grid-cols-2 lg:grid-cols-5 mx-5 lg:mx-0">
         {books?.map((book) => (
           <div key={book.title} className="flex flex-col overflow-hidden rounded-md bg-slate-200/50 dark:bg-slate-800 shadow-md cursor-pointer hover:-translate-y-1 hover:scale-110 transition duration-300 ease-in-out">
             <div onClick={()=>bookDetails(book.isbn13)} className="h-50 overflow-hidden">
-              <img className="h-50 w-50 object-fit  scale-[1.7] lg:scale-150" src={book.image} alt="" />
+              <img className="h-50 w-50 object-fit scale-[1.7] lg:scale-150" src={book.image} alt="" />
             </div>
             <div onClick={()=>bookDetails(book.isbn13)} className="flex flex-1 flex-col justify-between p-6">
               <div className="flex-1">
@@ -37,7 +37,7 @@ export default function BooksCard({books, heading}) {
               </div>
              
             </div>
-            <div className="py-2 bg-indigo-600 dark:bg-indigo-400 text-slate-100 dark:text-white">
+            <div className="py-2 bg-indigo-600 hover:bg-indigo-700 dark:bg-indigo-500 dark:hover:bg-indigo-600 text-slate-100 dark:text-white hover:font-bold">
               <div className="flex flex-row justify-center gap-4 items-center"> 
                 <p className="text-md">Add To Bag</p> 
                 <ShoppingBagIcon className="w-5 h-5 inline-block"></ShoppingBagIcon>
--- pages/books/search/[id].js
+++ pages/books/search/[id].js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 
+import { ShoppingBagIcon } from "@heroicons/react/24/outline";
 import { data } from "autoprefixer";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -12,6 +13,10 @@ import Spinner from "../../../components/common/Spinner";
     const [pageNo, setPageNo] = useState(1)
     const router = useRouter()
     const bookName = router.query.id
+
+    const bookDetails = (bookId) => {
+      router.push(`/books/${bookId}`)
+    }
 
     useEffect(()=> {
         fetch(`https://api.itbook.store/1.0/search/${bookName}/${pageNo}`)
@@ -43,25 +48,31 @@ import Spinner from "../../../components/common/Spinner";
       >
          Page: {pageNo} {"  |  "}Books:  {((pageNo -1)*10) + 1} to {((pageNo -1)*10) + remaining } out of {books?.total}
       </button>
-      <div className="grid gap-16 lg:gap-10 lg:grid-cols-5 mx-5 lg:mx-0">
+      <div className="grid gap-20 lg:gap-16 md:grid-cols-2 lg:grid-cols-5 mx-5 lg:mx-0">
         {books.books?.map((book) => (
-          <Link href={`/books/${book.isbn13}`} key={book.title} className="flex flex-col overflow-hidden rounded-md bg-slate-200/50 dark:bg-slate-800 shadow-md cursor-pointer hover:-translate-y-1 hover:scale-90 transition duration-300 ease-in-out">
-            <div className="h-50 overflow-hidden">
-              <img className="h-50 w-50 object-fit  scale-[1.7] lg:scale-150" src={book.image} alt="" />
-            </div>
-            <div className="flex flex-1 flex-col justify-between p-6">
-              <div className="flex-1">
-                <p className="text-sm lg:text-md font-bold text-indigo-600 dark:text-indigo-400">
-                {book?.price === "$0.00" ? "Free" : book?.price}
-                </p>
-                <div className="mt-2 block">
-                  <p className="text-xl font-semibold ">{book.title}</p>
-                  {/* <p className="mt-3 text-base text-gray-500">{!book?.subtitle ? "Unknown Category" : book?.subtitle}</p> */}
-                </div>
+          <div key={book.title} className="flex flex-col overflow-hidden rounded-md bg-slate-200/50 dark:bg-slate-800 shadow-md cursor-pointer hover:-translate-y-1 hover:scale-110 transition duration-300 ease-in-out">
+          <div onClick={()=>bookDetails(book.isbn13)} className="h-50 overflow-hidden">
+            <img className="h-50 w-50 object-fit  scale-[1.7] lg:scale-150" src={book.image} alt="" />
+          </div>
+          <div onClick={()=>bookDetails(book.isbn13)} className="flex flex-1 flex-col justify-between p-6">
+            <div className="flex-1">
+              <p className="text-sm lg:text-md font-bold text-indigo-600 dark:text-indigo-400">
+              {book?.price === "$0.00" ? "Free" : book?.price}
+              </p>
+              <div className="mt-2 block">
+                <p className="text-xl font-semibold ">{book.title.length > 24 ? book.title.slice(0, 24) + "..." : book.title }</p>
+                {/* <p className="mt-3 text-base text-gray-500">{!book?.subtitle ? "Unknown Category" : book?.subtitle}</p> */}
               </div>
-              
             </div>
-          </Link>
+           
+          </div>
+          <div className="py-2 bg-indigo-600 hover:bg-indigo-700 dark:bg-indigo-500 dark:hover:bg-indigo-600 text-slate-100 dark:text-white hover:font-bold">
+            <div className="flex flex-row justify-center gap-4 items-center"> 
+              <p className="text-md">Add To Bag</p> 
+              <ShoppingBagIcon className="w-5 h-5 inline-block"></ShoppingBagIcon>
+            </div>
+            </div>
+        </div>
         ))}
         </div>
         <div className="flex flex-1 justify-between my-10 items-center">
